9aa12c64ff2d340c2be9070087e80d595b6afa06
Tharun .s <161991385+Tharuns568@users.noreply.github.com>
2024-04-11 19:32:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -27,13 +27,13 @@ function addBtn() {
     <h2 >${bookTitle.value} </h2>
     <h4 >${bookAuthor.value}</h4>
     <p >${bookDesc.value}</p>
-    <button onclick="deleteBtn()">Delete</button>`;
+    <button onclick="deleteBtn(event)">Delete</button>`;
 
   mainContainer.append(newDiv);
   popup.style.display = "none";
   overlay.style.display = "none";
 }
 
-function deleteBtn() {
-  bookContainer.style.display = "none";
+function deleteBtn(event) {
+  event.target.parentElement.remove();
 }
