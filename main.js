474e5940418ca1e5368837ff8a3dc2484a677611
Tharun .s <161991385+Tharuns568@users.noreply.github.com>
2024-04-11 18:44:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+var popup = document.getElementById("popup");
+var overlay = document.getElementById("overlay");
+
+var bookContainer = document.getElementById("bookCont");
+var mainContainer = document.getElementById("main-cont");
+
+var bookTitle = document.getElementById("title");
+var bookAuthor = document.getElementById("author");
+var bookDesc = document.getElementById("descripFeild");
+
+
+function show() {
+  popup.style.display = "block";
+  overlay.style.display = "block";
+}
+
+function cancelBtn() {
+  popup.style.display = "none";
+  overlay.style.display = "none";
+}
+
+function addBtn() {
+  var newDiv = document.createElement("div");
+  newDiv.setAttribute("id", "bookCont");
+  newDiv.innerHTML = `
+    <h2 >${bookTitle.value} </h2>
+    <h4 >${bookAuthor.value}</h4>
+    <p >${bookDesc.value}</p>
+    <button onclick="deleteBtn()">Delete</button>`;
+
+  mainContainer.append(newDiv);
+  popup.style.display = "none";
+  overlay.style.display = "none";
+}
+
+function deleteBtn() {
+  bookContainer.style.display = "none";
+}
